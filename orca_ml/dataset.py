4defcfeea49543385564b2987c44be3f6f2caeff
Patrick Pastore <patrick.j.pastore@gmail.com>
2025-02-04 09:26:51 +0000

    Comment out cookie-cutter components

--- orca_ml/dataset.py
+++ orca_ml/dataset.py
@@ -1,29 +1,29 @@
-from pathlib import Path
+# from pathlib import Path
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+# import typer
+# from loguru import logger
+# from tqdm import tqdm
 
-from orca_ml.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+# from orca_ml.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
-app = typer.Typer()
-
-
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+# app = typer.Typer()
 
 
-if __name__ == "__main__":
-    app()
+# @app.command()
+# def main(
+#     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
+#     input_path: Path = RAW_DATA_DIR / "dataset.csv",
+#     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
+#     # ----------------------------------------------
+# ):
+#     # ---- REPLACE THIS WITH YOUR OWN CODE ----
+#     logger.info("Processing dataset...")
+#     for i in tqdm(range(10), total=10):
+#         if i == 5:
+#             logger.info("Something happened for iteration 5.")
+#     logger.success("Processing dataset complete.")
+#     # -----------------------------------------
+
+
+# if __name__ == "__main__":
+#     app()
